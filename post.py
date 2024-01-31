588ac734aaf74a185d16cff9ffd90489ee33cdee
Juan Carlos Solórzano <jcarlossolc@gmail.com>
2024-01-31 14:03:20 -0600

    Adjusted API Tests to functions to use later inside the bot

--- post.py
+++ post.py
@@ -2,37 +2,56 @@ import os
 from dotenv import load_dotenv
 import requests
 import time
+import json
 
 load_dotenv() 
 RapidAPIKey = os.environ.get('X-RapidAPI-Key')
 
-url = "https://postal-ninja.p.rapidapi.com/v1/track"
+def createTracking(trackCode):
+    url = "https://postal-ninja.p.rapidapi.com/v1/track"
 
-payload = { "trackCode": "ENTER_TRACKING_CODE_HERE" }
-headers = {
+    payload = { "trackCode": trackCode }
+    headers = {
 	"content-type": "application/x-www-form-urlencoded",
 	"Accept": "application/json; charset=UTF-8",
 	"Content-Type": "application/x-www-form-urlencoded",
 	"X-RapidAPI-Key": RapidAPIKey,
 	"X-RapidAPI-Host": "postal-ninja.p.rapidapi.com"
-}
+    }
+    response = requests.post(url, data=payload, headers=headers)
 
-response = requests.post(url, data=payload, headers=headers)
-
-print(response.json()['pkgId'])
-pkgId = response.json()['pkgId']
-
-url = "https://postal-ninja.p.rapidapi.com/v1/track/{pkgId}"
-
-querystring = {"await":"false","lang":"AS_IS"}
-
-headers = {
-	"Accept": "application/json; charset=UTF-8",
-	"X-RapidAPI-Key": RapidAPIKey,
-	"X-RapidAPI-Host": "postal-ninja.p.rapidapi.com"
-}
+    print(response.json()['pkgId'])
+    packageId = response.json()['pkgId']
+    return packageId
 
 time.sleep(2)
-response = requests.get(url, headers=headers, params=querystring)
 
-print(response.json())
+def getTrack(packageId):
+    url = f"https://postal-ninja.p.rapidapi.com/v1/track/{packageId}"
+    querystring = {"await":"false","lang":"AS_IS"}
+    headers = {
+        "Accept": "application/json; charset=UTF-8",
+        "X-RapidAPI-Key": RapidAPIKey,
+        "X-RapidAPI-Host": "postal-ninja.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+
+    # Initialize an empty dictionary to store the events
+    events_dict = {}
+
+    # Loop over all events
+    i = 0
+    for event in data['pkg']['events']:
+        # Use the event's id as the key and a dictionary of "dt" and "dsc" as the value
+        #events_dict[event['id']] = {'dt': event['dt'], 'dsc': event['dsc']}
+        events_dict['event_id'] = i
+        events_dict['dt'] = event['dt']
+        events_dict['dsc'] = event['dsc']
+        i += 1
+
+    return events_dict
+
+
+
+
